53686d4334d100dbae86071e125d790b99d48d27
dimlil <d.v.vassilev@gmail.com>
2021-11-19 01:10:53 +0200

    feat/ connected to mongoDB and added .env file

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import router from "./routes/index.mjs";
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use('/', router)
+app.use('/', router);
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    }
+})  
 
 const PORT = process.env.PORT || 5000
 
