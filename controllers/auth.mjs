719f9cc6e92209183d2bc134be9d9c05dca9df1a
dimlil <d.v.vassilev@gmail.com>
2021-11-20 00:17:55 +0200

    feat/ added verification is email and password are in >> DB and is request authorized

--- controllers/auth.mjs
+++ controllers/auth.mjs
@@ -1,19 +1,29 @@
-import {body} from 'express-validator'
+import { body } from "express-validator";
+import users from "../models/user.mjs";
+import bcrypt from "bcrypt";
 
 export const verifyUser = [
   body(
     "email",
     "The email should be in following format (mailbox @ domainname)"
-  ).isEmail(),
+  )
+    .isEmail()
+    .custom(async (email) => {
+      const user = await users.findOne({ email });
+      if (!user) {
+        throw new Error("Email is not found");
+      }
+      return true;
+    }),
   body(
     "password",
     "The password should be at least 4 characters long and should consist only english letters and digits"
-  ).custom((value) => {
-    const regex = /[A-Za-z0-9]+/g;
-    if (value < 4 || !value.match(regex)) {
-      throw new Error(
-        `The password should be at least 4 characters long and should consist only english letters and digits`
-      );
+  ).custom(async (password,{req}) => {
+    const email= req.body.email;
+    const user = await users.findOne({ email });
+    const status = await bcrypt.compare(password, user.password);
+    if (!status) {
+      throw new Error("Wrong password");
     }
     return true;
   }),
---
+++ models/user.mjs
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const UserShcema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    }
+
+})
+
+export default mongoose.model('User', UserShcema);
