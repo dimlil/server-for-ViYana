893d53dd674557dc9e7fb1fffa3aa911939cc000
dimlil <d.v.vassilev@gmail.com>
2022-03-01 19:34:44 +0200

    feat/ added functionality to create post

---
+++ models/post.mjs
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+})
+export default mongoose.model('Post', PostSchema);
--- routes/index.mjs
+++ routes/index.mjs
@@ -3,14 +3,24 @@ import { validationResult } from "express-validator";
 import { verifyUser } from "../controllers/auth.mjs";
 import { login } from "../controllers/login.mjs";
 import { loginVerification } from "../verifications/login.mjs";
+import Post from '../models/post.mjs'
 const router = Router();
 
 router.get("/", async (req, res) => {
   res.send("Hello");
 });
-router.post("/api/create", verifyUser, (req, res) => {
- 
-  res.send("ok");
+router.post("/api/create", verifyUser, async (req, res) => {
+  const { title, content } = req.body;
+  const newPost= new Post({
+    title,
+    content
+  })
+  try {
+    await newPost.save()
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+  res.status(201).send("ok");
 });
 router.post("/api/login", loginVerification, login, (req, res) => {
   const errors = validationResult(req);
